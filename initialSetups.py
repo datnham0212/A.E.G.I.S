c567ce228265f9d13538969538c49a6999a1fd75
datnham0212 <datnham0212@gmail.com>
2023-01-11 09:58:55 +0700

    4th

--- initialSetups.py
+++ initialSetups.py
@@ -4,25 +4,39 @@ engine = pt.init()
 
 def run():
     aegis_voice()
-    aegis_greet()
-    engine.runAndWait()
+    while True: 
+        aegis_greet()
+        engine.runAndWait()
+        
+        user_input = input("Type 'exit' to stop, or press Enter to continue: ")
+        if user_input.lower() == 'exit':
+            aegis_stop()
+            break
+            
 
 def aegis_voice():    
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)
     engine.setProperty('rate', 150)
     
-def aegis_greet():
-    engine.say(f"{getPartOfDay()} master Dat, how may I assist you?")
 
-def getPartOfDay():
+def aegis_greet():
     import datetime
     now = datetime.datetime.now()
     hour = now.hour
-    if hour < 12:
-        return "Good Morning"
-    elif hour < 18:
-        return "Good Afternoon"
-    else:
-        return "Good Evening"
+    if hour in range(6, 12):
+        temp = "Good Morning"
+    elif hour in range(12, 19):
+        temp = "Good Afternoon"
+    elif hour in range(19, 24):
+        temp = "Good Evening"
 
+    engine.say(f"{temp} master Dat, how may I assist you?") if hour in range(6, 24) else engine.say("What concerns are you facing at this late hour, master Dat?")
+
+
+
+def aegis_stop():
+    engine.say(f"Very well sir.")
+    engine.runAndWait()
+    
+    
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-import initialSetups as aegis
-
-if __name__ == '__main__':
-    aegis.run()
